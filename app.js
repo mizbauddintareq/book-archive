8df11e0b1653582ba6c406e12a03f870a8c019b2
mizbauddintareq <mizbauddintareq@gmail.com>
2021-09-02 14:48:59 +0600

    add error text

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const getBookDetails = () => {
     const searchValue = searchInput.value;
     searchInput.value = '';
 
+    // // toggle & spinner
     toggleSpinner('block');
     toggleSearchResult('none');
 
@@ -23,21 +24,51 @@ const getBookDetails = () => {
 
 // display data on ui
 const displayBookDetails = details => {
-    const resultContainer = document.getElementById('result-container')
-    resultContainer.textContent = '';
-    const totalResult = document.getElementById('total-results');
-    totalResult.textContent = '';
-    const div = document.createElement('div');
-    div.innerHTML =`
-        <h1 class="text-center text-danger">Total <span class="fw-bold">${details.numFound} </span> Books Found
-        Related To Your Search
-        </h1>
-    `;
-    totalResult.appendChild(div)
+
+    // error message for invalid or blank search
+    const errorText = document.getElementById('error-text');
+    if (details.numFound === 0){
+        errorText.innerText = '';
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <div class="card text-center text-danger border-danger">
+                <div class="card-header">
+                    This Is An Error Alert!!!
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">This Error Is Due To Either Blank Search Or Invalid Search.</h5>
+                </div>
+                <div class="card-footer text-muted">
+                    Please Enter A Book Name So That We Can Help You To Find It. Thank You!!!
+                </div>
+            </div>`;
+
+        errorText.appendChild(div);
+        toggleSpinner('none');
+        
+    }
+
+    else{
+        // numbers of total results found
+        errorText.innerText = '';
+        const resultContainer = document.getElementById('result-container')
+        resultContainer.textContent = '';
+        const totalResult = document.getElementById('total-results');
+        totalResult.textContent = '';
+        const div = document.createElement('div');
+        div.innerHTML =`
+            <h2 class="text-center text-dark">Total <span class="fw-bold">${details.numFound} </span> Books Found
+            Related To Your Search
+            </h2>`;
+    
+        totalResult.appendChild(div);
+        
+
+    // shown search results by card
     details.docs.forEach(detail => {
         const col = document.createElement('col');
         col.innerHTML = `
-            <div class="card h-100">
+            <div class="card h-100 shadow rounded border border-light">
                 <img src="https://covers.openlibrary.org/b/id/${detail.cover_i}-M.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h3 class="card-title"><span class="fw-bold">Book Name</span>: ${detail.title}</h3>
@@ -45,12 +76,15 @@ const displayBookDetails = details => {
                     <h4><span class="fw-bold">Publisher</span>: ${detail.publisher ? detail.publisher : '<span class="text-danger">Not Found</span>'}</h4>
                     <h5 class="card-title"><span class="fw-bold">First Published On</span>: ${detail.first_publish_year ? detail.first_publish_year : '<span class="text-danger">Not Found</span>'}</h5>
                 </div>
-            </div>
-        `;
-        resultContainer.appendChild(col);
-        toggleSpinner('none');
-        toggleSearchResult('block');
-    })
+            </div>`;
+        
+            resultContainer.appendChild(col);
+
+            // toggle & spinner
+            toggleSpinner('none');
+            toggleSearchResult('block');
+        });
+    }
 };
 
 
